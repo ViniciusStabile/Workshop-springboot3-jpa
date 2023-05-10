30706c11b43f853fe2b1771bc21a62c17baf7352
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-05-10 10:42:52 -0300

    Product

--- src/main/java/com/projetoSpring/course/entities/Category.java
+++ src/main/java/com/projetoSpring/course/entities/Category.java
@@ -1,25 +1,30 @@
 package com.projetoSpring.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
+public class Category implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	@Transient
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 
@@ -44,6 +49,10 @@ public class Category implements Serializable{
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
