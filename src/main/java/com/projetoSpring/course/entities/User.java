35909b7c1c3b3632300fa934547faf2829929787
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-05-09 21:20:04 -0300

    Order, instant, ISO 8601

--- src/main/java/com/projetoSpring/course/entities/User.java
+++ src/main/java/com/projetoSpring/course/entities/User.java
@@ -1,21 +1,23 @@
 package com.projetoSpring.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-
-@Entity 
+@Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
-	
+public class User implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -23,9 +25,12 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -34,6 +39,10 @@ public class User implements Serializable{
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public Long getId() {
@@ -92,8 +101,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
 
-	
-	
 }
