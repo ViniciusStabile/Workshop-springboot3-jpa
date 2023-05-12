22fc638ed3c9f328d9cff82761a907070ab2b336
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-05-12 08:40:56 -0300

    User update

--- src/main/java/com/projetoSpring/course/services/UserService.java
+++ src/main/java/com/projetoSpring/course/services/UserService.java
@@ -11,19 +11,19 @@ import com.projetoSpring.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
@@ -31,5 +31,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
+
 }
